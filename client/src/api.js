5c5ceb2877eb6690345362c1f4bfd177459f8b29
SamanthaTsen <pinkkuang@hotmail.com>
2025-09-05 16:41:25 -0500

    update the api setting

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: https://task-manager-saas.onrender.com || 'http://localhost:5000',
 });
 API.interceptors.request.use((req) => { 
   const token = localStorage.getItem('token');
